8c3029cd675cbff70148a8150fe18ec6b5858aaa
Ddanilo9 <interiordanilo3@gmail.com>
2022-10-12 14:36:03 +0200

    tab migrations

---
+++ database/migrations/2022_10_12_121935_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda',100);
+            $table->string('stazione_di_partenza',50);
+            $table->string('stazione_di_arrivo',50);
+            $table->date('orario_di_partenza');
+            $table->date('orario_di_arrivo');
+            $table->integer('codice_treno');
+            $table->tinyInteger('numero_carrozze');
+            $table->unsignedTinyInteger('in_orario')->default(1);
+            $table->unsignedTinyInteger('cancellato')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
