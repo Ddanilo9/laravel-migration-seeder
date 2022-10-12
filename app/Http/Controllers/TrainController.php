a7e4509c2fb5e2c21a8220e413e88c3b3a7778ab
Ddanilo9 <interiordanilo3@gmail.com>
2022-10-12 15:15:03 +0200

    bug seeder Train

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+// use App\Train;
+
+
+class TrainController extends Controller
+{
+    public function index() {
+
+        // $trains = Train::all();
+        // dd($trains);
+        return view('show', );
+    }
+}
+
+
--- database/seeds/TrainSeeder.php
+++ database/seeds/TrainSeeder.php
@@ -11,6 +11,33 @@ class TrainSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $trains = [
+            [
+                'azienda' => 'Trenitalia',
+                'stazione_di_partenza' => 'Roma',
+                'stazione_di_arrivo' => 'Milano',
+                'orario_di_partenza' => '14:25',
+                'orario_di_arrivo' => '17:30',
+                'codice_treno' => 55145496,
+                'numero_carrozze' => 25,
+                'in_orario' => 1,
+                'cancellato' => 0
+            ],
+        ];
+
+        foreach ($trains as $train) {
+            $newTrain = new Train();
+            $newTrain->azienda = $train['azienda'];
+            $newTrain->stazione_di_partenza = $train['stazione_di_partenza'];
+            $newTrain->stazione_di_arrivo = $train['stazione_di_arrivo'];
+            $newTrain->orario_di_partenza = $train['orario_di_partenza'];
+            $newTrain->orario_di_arrivo = $train['orario_di_arrivo'];
+            $newTrain->codice_treno = $train['codice_treno'];
+            $newTrain->azienumero_carrozzenda = $train['numero_carrozze'];
+            $newTrain->in_orario = $train['in_orario'];
+            $newTrain->cancellato = $train['cancellato'];
+
+            $newTrain->save();
+        }
     }
 }
